23f71ee37365d088500343680426d0089217f3a0
Elijah Davis <elijah.davis@stu.daviess.kyschools.us>
2026-04-01 21:06:32 +0000

    Commit

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,38 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
+		
+		int bottom = 0;
+		int top = ray.length-1;
+		int middle = (bottom+top)/2;
+		//Boolean sentinal
+		boolean foundIt = false;
 
+		while(bottom <= top && !foundIt)
+		{
+			//Resetting the middle after every loop since the top or bottom value changes with each loop	
+			middle = (bottom + top) / 2;
+			//If the middle value is less than the index
+        	if(ray[middle] < item)
+        	{
+				//Change the bottom to the middle value plus 1
+            	bottom = middle + 1;
+        	}
+			//Else the middle value is greater than the index
+        	else if(ray[middle] > item)
+        	{
+				//Change the top to the middle value minus 1
+            	top = middle - 1;
+        	}
+			//Else we found the index
+        	else
+        	{
+				//Return the index 
+            	return middle;
+        	}
+		}
+		//If the value doesn't exist at all than return -1
+		return -1;
+		
 	}
 }
--- BinarySearchTester.java
+++ BinarySearchTester.java
@@ -5,14 +5,17 @@ public class BinarySearchTester
    public static void main( String args[] )
    {
 		int[] array = {39,6,11,23,18,3,20,5,57,15,8};
-
+		
 		System.out.println("before calling binarySearch");
 		System.out.println(Arrays.toString(array));
-
+		//Sorts the array in ascending numerical order
+		Arrays.sort(array);
+		//re prints the array, but in sorted order now
+		System.out.println(Arrays.toString(array));
 		System.out.println("looking for 8 :: "+	BinarySearch.binarySearch(array,8));
-		System.out.println("looking for 39 :: "+	BinarySearch.binarySearch(array,39));
-		System.out.println("looking for 15 :: "+	BinarySearch.binarySearch(array,15));
+		System.out.println("looking for 39 :: "+BinarySearch.binarySearch(array,39));
+		System.out.println("looking for 15 :: "+BinarySearch.binarySearch(array,15));
 		System.out.println("looking for 5 :: "+	BinarySearch.binarySearch(array,5));
-		//System.out.println("looking for 10 :: "+ BinarySearch.binarySearch(array,10));
+		System.out.println("looking for 10 :: "+ BinarySearch.binarySearch(array,10));
 	}
 }
